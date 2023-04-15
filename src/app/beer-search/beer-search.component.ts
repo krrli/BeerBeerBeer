8c1989adf2667a369094145e2cb772f0f48d22ca
Carla Iten <carlaiten@gmail.com>
2023-04-15 14:54:55 +0200

    added arrow up, down and enter listeners to result list

--- src/app/beer-search/beer-search.component.ts
+++ src/app/beer-search/beer-search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Beer } from '../utils/types';
 
 @Component({
   selector: 'beer-search',
@@ -6,21 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./beer-search.component.scss'],
 })
 export class BeerSearchComponent {
-  searchTerm = "";
-  beers$: Array<String> = new Array("hoi", "Tschüss", "byebye");
-
+  searchTerm = '';
+  beers$: Array<Beer> = new Array(
+    { id: 0, name: 'hoi'},
+    { id: 1, name: 'hoi 2'},
+    { id: 2, name: 'blubbsi'}
+  );
+  activeBeer$?: Beer;
+  activeIndex: number = 0;
 
   onKeydown($event: Event) {
-    console.log('key down');
+    if(this.activeIndex < this.beers$.length-1){
+      this.activeIndex++;
+    }
+    else{
+      this.activeIndex = 0;
+    }
+    this.activeBeer$ = this.beers$[this.activeIndex];
   }
   onKeyup($event: Event) {
-    console.log('key up');
+    if(this.activeIndex === 0){
+      this.activeIndex = this.beers$.length-1;
+    }else{
+      this.activeIndex--;
+    }
+    this.activeBeer$ = this.beers$[this.activeIndex];
   }
   onEnter($event: Event) {
-    console.log('enter');
+    this.activeBeer$ = this.beers$[this.activeIndex];
+  }
+
+  onClickSelectBeer(beer: Beer){
+    this.activeBeer$ = beer;
   }
 
   search(term: string): void {
     this.searchTerm = term;
+    this.activeBeer$ = this.beers$[this.activeIndex];
   }
 }
