35d347a66dbf1d95d01070522d251e6b8803e326
Carla Iten <carlaiten@gmail.com>
2023-04-15 17:25:45 +0200

    cleanup unused files and imports

--- src/app/utils/beer-service.service.ts
+++ src/app/utils/beer-service.service.ts
@@ -9,20 +9,14 @@ import { IBeer } from './types';
   providedIn: 'root',
 })
 export class BeerService {
-  //private beers = new BehaviorSubject<IBeer[]>(null);
-  //beers$: Observable<IBeer[]> = this.beers.asObservable();
   private APIPath: string = 'https://api.punkapi.com/v2/beers';
-  private maxPerLoad = 12;
-  private apiString: any;
   private maxPerPage: number = 36;
 
 
   constructor(private http: HttpClient) {
-    //this.GetAllBeers();
+
   }
 
-  // GET
-  // All beers set to apiString
   GetAllBeers(page: number = 1) : Observable<IBeer[]> {
     return this.http
       .get<IBeer[]>(`${this.APIPath}?page=${page}&per_page=${this.maxPerPage}`)
